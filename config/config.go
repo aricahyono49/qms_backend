121079c16fd47a973faebecb21616cb82849a783
Ari Cahyono <cahyono.ari80@gmail.com>
2021-10-18 15:50:52 +0700

    refactor : db

--- config/config.go
+++ config/config.go
@@ -2,28 +2,12 @@ package config
 
 import (
 	"database/sql"
-	"fmt"
-
 	_ "github.com/go-sql-driver/mysql"
-)
-
-const (
-	username string = "root"
-	password string = ""
-	database string = "rakamin?parseTime=True"
-	
-	// username string = "backend-c-queue-management@tcp(35.201.240.187:3306)"
-	// password string = "9MghTbFu5NCRt6lc"
-	// database string = "backend-c-queue-management?parseTime=True"
-)
-
-var (
-	dsn = fmt.Sprintf("%v:%v@/%v", username, password, database)
 )
 
 // HubToMySQL
 func MySQL() (*sql.DB, error) {
-	db, err := sql.Open("mysql", dsn)
+	db, err := sql.Open("mysql", "root@tcp(localhost:3306)/rakamin")
 
 	if err != nil {
 		return nil, err
